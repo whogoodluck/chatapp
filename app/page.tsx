56bfa5ae0a7367d215d82f2fb31f8e331720b326
sunny <goodluck14115@gmail.com>
2025-09-02 18:44:42 +0530

    Setup prettier for better code formating

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-3xl font-bold text-red-600">Home</h1>
+    <div className='flex h-screen items-center justify-center'>
+      <h1 className='text-3xl font-bold text-red-600'>Home</h1>
     </div>
-  );
+  )
 }
